b2ffc294abc908d1e51920973cc2ca6be89ac2d6
Fuji Robledo <ProgrammingPenguin@Fujis-MacBook-Pro.local>
2016-11-14 19:51:58 -0700

    FINAL Milestone 2

--- Grad Project/Milestone 2/iCAREMilestone2/app/src/main/java/com/example/programmingpenguin/icaremilestone2/MainActivity.java
+++ Grad Project/Milestone 2/iCAREMilestone2/app/src/main/java/com/example/programmingpenguin/icaremilestone2/MainActivity.java
@@ -1,6 +1,11 @@
 /*The purpose of this app is to obtain information from a server to display a message.
 * The user will then be able to act on the message by pressing two buttons.
-* The pressing of the two buttons will send data back to a google Spreadsheet.*/
+* The pressing of the two buttons will send data back to a google Spreadsheet.
+*
+* References:
+* http://www.wikihow.com/Execute-HTTP-POST-Requests-in-Android
+* https://developer.android.com/reference/java/net/HttpURLConnection.html
+* */
 
 package com.example.programmingpenguin.icaremilestone2;
 
@@ -8,6 +13,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,22 +30,74 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void getMessage(View view) {
-        //When the user clicks on the button, a message will be display
-        //The information for this textview will be retrieved from the google spreadsheet
-        //This is the link to the spreadsheet: https://docs.google.com/spreadsheets/d/10CDo6pLTbqB6w-h_phoc2F3CmscuAzZ3M2DqWvXlofw/pubhtml
-        //A random number generator will select which (out of 3 messages) to show
 
+    public void obtainServerInfo() {
+
+        //try to access server
+        try{
+            URL url = new URL("http://creative.colorado.edu/~beardn/example.php");
+            HttpURLConnection info = (HttpURLConnection) url.openConnection();
+
+
+            //To post output request and handle exceptions
+            info.setRequestMethod("POST");
+            info.setRequestProperty("Key","Value"); //how do you know key/value
+            info.setDoOutput(true);
+
+            //Once connected, use getInputStream (https://developer.android.com/reference/java/net/URLConnection.html#getInputStream())
+            //to get data from server - set this data to be displayed at label
+        }
+
+        //handles incorrectly entered URL
+        catch(MalformedURLException error){ //is this needed?
+
+        }
+        //handles URL access timeout
+        catch(SocketTimeoutException error){
+
+        }
+        //handles input and output errors
+        catch(IOException error){
+
+        }
+
+        finally {
+            if(info != null){
+                info.disconnect();
+            }
+        }
+
+        //To display message
         TextView displayMessage = (TextView) findViewById(R.id.textView2);
-        displayMessage.setText("Message generated randomly by google Spreadsheet");
-
-        //Every time the user clicks on the button, the data from the spreadsheet will be retrieved and randomly shown in text view
+        displayMessage.setText("message obtained from server");
 
     }
 
-    //Another function will determine what happens when either the red or green button are clicked
-    //If the red button is clicked - then the message of "please wait" will be sent and stored in google spreadsheet
-    //If the green button is clicked - then the message of "on my way" will be sent and stored in google spreadsheet
+    public void pleaseWait(View view){
+        //If the red button is clicked - then the message of "please wait" will be sent
+
+        /*
+        Modify the code below to deal with uploading message information to server
+
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            urlConnection.setDoOutput(true);
+            urlConnection.setChunkedStreamingMode(0);
+
+            OutputStream out = new BufferedOutputStream(urlConnection.getOutputStream());
+            writeStream(out);
+
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            readStream(in);
+        } finally {
+            urlConnection.disconnect();*/
+        }
+    }
+
+    public void onMyWay(View view){
+        //If the green button is clicked - then the message of "on my way" will be sent and stored in google spreadsheet
+    }
+
 }
 
 
